365938ed99b2e464588822cada5b2e74c8feb329
osintlarp <csintlarp@gmail.com>
2025-10-24 19:53:51 +0200

    Update instagram.py

--- instagram.py
+++ instagram.py
@@ -1,127 +1,107 @@
 import json
-import re
-from collections import Counter
+import collections
+from bs4 import BeautifulSoup
 from utils import try_request, get_user_agent
 
-def sort_list(item_list):
-    if not item_list:
-        return {}
-    return dict(Counter(item_list).most_common())
+collections.Callable = collections.abc.Callable
 
-def find_details(text_blob):
-    emails = re.findall(r'[\w\.-]+@[\w\.-]+\.\w+', text_blob)
-    tags = re.findall(r'#(\w+)', text_blob)
-    mentions = re.findall(r'@(\w+)', text_blob)
-    
-    return {
-        'emails': list(set(emails)),
-        'tags': sort_list(tags),
-        'mentions': sort_list(mentions)
-    }
+def _tiny_url(url):
+    apiurl = "http://tinyurl.com/api-create.php?url="
+    r, err = try_request("get", apiurl + url)
+    if not err and r:
+        return r.text
+    return url
+
+def _extract_hash_tags(stri):
+    return [part[1:] for part in stri.split() if part.startswith("#")]
 
 def get_instagram_info(username):
-    url = f'https://www.instagram.com/{username}/?__a=1'
-    headers = {
-        'User-Agent': get_user_agent(),
-        'Accept': 'application/json'
-    }
+    profile_url = f"https://www.instagram.com/{username}"
+    headers = {"User-Agent": get_user_agent()}
     
-    r, err = try_request("get", url, headers=headers)
-    
+    profile_page_html, err = try_request("get", profile_url, headers=headers)
     if err:
         return {'error': err}
-    if not r:
-        return {'error': 'Failed to retrieve Instagram data'}
+    if not profile_page_html:
+        return {'error': 'Failed to retrieve Instagram profile page.'}
 
+    soup = BeautifulSoup(profile_page_html.text, "html.parser")
+    more_data_scripts = soup.find_all("script", attrs={"type": "text/javascript"})
+    
+    data = None
     try:
-        data = r.json()
-    except json.JSONDecodeError:
-        return {'error': 'Failed to decode Instagram response. The API may be blocked or changed.'}
+        for script in more_data_scripts:
+            if script.string and script.string.startswith('window._sharedData ='):
+                json_text = script.string[21:].strip(';')
+                data = json.loads(json_text)
+                break
+        
+        if not data:
+             return {'error': 'Could not find profile data JSON on page. Instagram structure may have changed.'}
+        
+        p_data = data["entry_data"]["ProfilePage"][0]["graphql"]["user"]
+    except Exception as e:
+        print(f"Error parsing Instagram JSON: {e}")
+        return {'error': 'Failed to parse Instagram data. Page structure may have changed.'}
 
-    if 'graphql' not in data or 'user' not in data['graphql']:
-        return {'error': 'User not found or invalid response structure'}
-
-    js = data['graphql']['user']
-    
-    is_private = js.get('is_private', False)
-    
-    usrinfo = {
-        'username': js.get('username'),
-        'user_id': js.get('id'),
-        'name': js.get('full_name'),
-        'followers': js.get('edge_followed_by', {}).get('count'),
-        'following': js.get('edge_follow', {}).get('count'),
-        'posts_media_count': js.get('edge_owner_to_timeline_media', {}).get('count'),
-        'posts_video_count': js.get('edge_felix_video_timeline', {}).get('count'),
-        'reels_count': js.get('highlight_reel_count'),
-        'bio': js.get('biography', '').replace('\n', ', '),
-        'external_url': js.get('external_url'),
-        'is_private': is_private,
-        'is_verified': js.get('is_verified'),
-        'profile_pic_url': js.get('profile_pic_url_hd'),
-        'is_business_account': js.get('is_business_account'),
-        'is_joined_recently': js.get('is_joined_recently'),
-        'business_category_name': js.get('business_category_name'),
-        'category_enum': js.get('category_enum'),
-        'has_guides': js.get('has_guides'),
+    user_info = {
+        "username": p_data.get("username"),
+        "name": p_data.get("full_name"),
+        "url": f"instagram.com/{p_data.get('username')}",
+        "followers": p_data.get("edge_followed_by", {}).get("count"),
+        "following": p_data.get("edge_follow", {}).get("count"),
+        "posts": p_data.get("edge_owner_to_timeline_media", {}).get("count"),
+        "bio": p_data.get("biography", "").replace("\n", ", "),
+        "external_url": p_data.get("external_url"),
+        "is_private": p_data.get("is_private"),
+        "is_verified": p_data.get("is_verified"),
+        "profile_pic_url": _tiny_url(p_data.get("profile_pic_url_hd")),
+        "is_business_account": p_data.get("is_business_account"),
+        "connected_fb_page": p_data.get("connected_fb_page"),
+        "is_joined_recently": p_data.get("is_joined_recently"),
+        "business_category_name": p_data.get("business_category_name"),
     }
 
-    text_blob = str(data)
-    extracted_details = find_details(text_blob)
-    
-    usrinfo['extracted_emails'] = extracted_details['emails']
-    usrinfo['extracted_tags'] = extracted_details['tags']
-    usrinfo['extracted_mentions'] = extracted_details['mentions']
-    
+    raw_tags = []
+    tag_lis = []
     posts = []
-    if not is_private:
-        total_uploads = js.get('edge_owner_to_timeline_media', {}).get('count', 0)
-        max_posts_to_fetch = min(total_uploads, 12) 
-        
-        edges = js.get('edge_owner_to_timeline_media', {}).get('edges', [])
-        
-        for i in range(min(len(edges), max_posts_to_fetch)):
-            node = edges[i].get('node', {})
-            post_info = {}
-            child_media_list = []
-
-            post_details = {
-                'comments_count': node.get('edge_media_to_comment', {}).get('count'),
-                'comments_disabled': node.get('comments_disabled'),
-                'timestamp': node.get('taken_at_timestamp'),
-                'likes_count': node.get('edge_liked_by', {}).get('count'),
-                'location': node.get('location'),
-                'caption': node.get('edge_media_to_caption', {}).get('edges', [{}])[0].get('node', {}).get('text', ''),
-                'post_url': f"https://www.instagram.com/p/{node.get('shortcode')}/"
-            }
-
-            if 'edge_sidecar_to_children' in node:
-                for child_edge in node['edge_sidecar_to_children'].get('edges', []):
-                    child_node = child_edge.get('node', {})
-                    media_info = {
-                        'typename': child_node.get('__typename'),
-                        'id': child_node.get('id'),
-                        'shortcode': child_node.get('shortcode'),
-                        'dimensions': child_node.get('dimensions'),
-                        'image_url': child_node.get('display_url'),
-                        'is_video': child_node.get('is_video'),
-                        'accessibility_caption': child_node.get('accessibility_caption')
-                    }
-                    child_media_list.append(media_info)
-            else:
-                media_info = {
-                    'typename': node.get('__typename'),
-                    'id': node.get('id'),
-                    'shortcode': node.get('shortcode'),
-                    'dimensions': node.get('dimensions'),
-                    'image_url': node.get('display_url'),
-                    'is_video': node.get('is_video'),
-                    'accessibility_caption': node.get('accessibility_caption')
+    
+    if not user_info["is_private"]:
+        try:
+            for post_edge in p_data.get("edge_owner_to_timeline_media", {}).get("edges", []):
+                post = post_edge.get("node", {})
+                caption = ""
+                
+                try:
+                    caption_node = post.get("edge_media_to_caption", {}).get("edges", [{}])[0].get("node", {})
+                    caption = caption_node.get("text", "")
+                    raw_tags.append(_extract_hash_tags(caption))
+                except IndexError:
+                    pass
+                
+                post_data = {
+                    "picture_url": _tiny_url(post.get("thumbnail_resources", [{}])[0].get("src")),
+                    "caption": caption,
+                    "comments": post.get("edge_media_to_comment", {}).get("count"),
+                    "comments_disabled": post.get("comments_disabled"),
+                    "timestamp": post.get("taken_at_timestamp"),
+                    "likes": post.get("edge_liked_by", {}).get("count"),
+                    "location": post.get("location"),
+                    "accessibility_caption": post.get("accessibility_caption"),
+                    "post_url": f"https://instagram.com/p/{post.get('shortcode')}"
                 }
-                child_media_list.append(media_info)
-            
-            post_info['media'] = child_media_list
-            post_info['details'] = post_details
-            posts.append(post_info)
+                posts.append(post_data)
 
-    return {'user_info': usrinfo, 'posts': posts}
+            for tag_group in raw_tags:
+                tag_lis.extend(tag_group)
+            
+            common_tags = dict(collections.Counter(tag_lis).most_common(10))
+
+        except Exception as e:
+            print(f"Error parsing Instagram posts: {e}")
+            
+    return {
+        "user_info": user_info,
+        "most_used_tags": common_tags if 'common_tags' in locals() else {},
+        "posts": posts
+    }
