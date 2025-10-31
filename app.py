abac1a5190c1de2b86cf58443508f84e99b675ed
osintlarp <csintlarp@gmail.com>
2025-10-31 19:22:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_limiter import Limiter
+from flask_cors import cross_origin
 from flask_limiter.util import get_remote_address
 from flask_cors import CORS
 from functools import wraps
